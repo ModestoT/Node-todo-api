5ba67bb136f23027f42b7cfe7c16038fa2249e7c
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2017-10-31 14:44:26 -0500

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connect to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('59f8c4b57e3da7391080fd27')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch Todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch Todos', err);
+  // });
+  db.collection('Users').find({name: 'Tito'}).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch Users');
+  });
+  // db.close();
+});
